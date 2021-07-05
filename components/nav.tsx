cd41d3348283f0855b05745a1af9b126255d7818
janoshr <janos.hajdurafis@gmail.com>
2021-07-05 23:40:32 +0100

    static export enabled

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import logo from '../public/images/Yi0Byu2n_o.png'
+import logo from '../public/images/logo.png'
 
 export default function Nav(){
   return(
     <nav className="navbar navbar-expand-lg navbar-light shadow">
       <div className="container">
         <a className="navbar-brand">
-          <Image src={logo} alt="DeltaSqrd logo" width={32} height={32} />
+          <Image src={logo} alt="DeltaSqrd logo" width={32} height={32} unoptimized />
         </a>
         <ul className="navbar-nav">
           <li className="nav-item">
